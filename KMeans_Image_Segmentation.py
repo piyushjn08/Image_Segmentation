05691c360d8937fc00b790ba737e089054996cfa
piyushjn08 <35859644+piyushjn08@users.noreply.github.com>
2019-08-25 20:15:59 +0530

    Update KMeans_Image_Segmentation.py

--- KMeans_Image_Segmentation.py
+++ KMeans_Image_Segmentation.py
@@ -10,24 +10,39 @@ import cv2
 import time
 import numpy as np
 
+COLORS = [[255,0,0],
+          [0,255,0],
+          [0,0,255],
+          [255,255,0], #yello
+          [0,255,255], #Cyan
+          [255,0,255], #Magenta
+          [192,192,192], #Silver
+          [128,128,128], #Gray
+          [128,0,0], #Maroon
+          [128,128,0], #Olive
+          [0,128,0], #Green
+          [128,0,128], #Purple
+          [0,128,128], #Teal
+          [0,0,128]] #Navy
+
+#%% Read Image
 image = cv2.imread('C:\\Users\\Piyush\\Pictures\\Deadpool_Movie.jpg')
 image = cv2.bilateralFilter(image,15,75,75)
 h,w,c = image.shape
 image = cv2.resize(image, (256,256))
-image = cv2.bilateralFilter(image,15,75,75)
+
+
 h,w,c = image.shape
 copy = image.copy()
 print(image.shape)
 X = np.reshape(image, (h*w, c))
 print(X.shape)
 
-COLORS = [[255,0,0],
-          [0,255,0],
-          [0,0,255]]
 #%% K-Means
+n_clusters = 5
 print("Computing K-Means")
 start = time.time()
-kmeans = MiniBatchKMeans(n_clusters=3, random_state=0, batch_size=10000).fit(X)
+kmeans = MiniBatchKMeans(n_clusters=n_clusters, random_state=0, batch_size=10000).fit(X)
 print("Done Computing")
 print("time Taken:",time.time() - start)
 
@@ -36,9 +51,8 @@ print(kmeans.labels_)
 original = np.reshape(kmeans.labels_, (h,w))
 print(original.shape)
 
-copy[ original == 0] = COLORS[0]
-copy[ original == 1] = COLORS[1]
-copy[ original == 2] = COLORS[2]
+for i in range(n_clusters):
+    copy[ original == i] = COLORS[i]
 
 #%% Display Picture
 cv2.namedWindow("Original", cv2.WINDOW_NORMAL)
